d3ccc4dcc0599d694e441619e7406b67b53274a8
AnastasiaEmelianova <ae@qsa.ru>
2022-04-18 15:51:10 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ public:
     friend istream &operator>>(istream &istr, Matrix &A); //перегрузка оператора ввода
     friend ostream &operator<<(ostream &ostr, Matrix &A); //перегрузка оператора вывода
     friend Matrix operator+(Matrix &m1, Matrix &m2);      //перегрузка оператора плюс (бинарный)
-    friend Matrix operator-(Matrix &m1, Matrix &m2);      //перегрузка оператора минус (бинарный)
+    friend Matrix operator-(Matrix &m1, Matrix &m2);     //перегрузка оператора минус (бинарный)
     friend Matrix operator*(Matrix &m1, Matrix &m2);
 };
 
@@ -78,17 +78,19 @@ Matrix operator+(Matrix &m1, Matrix &m2) //перегрузка оператор
 
 Matrix operator-(Matrix &m1, Matrix &m2) //перегрузка оператора минус (бинарный)
 {
-    Matrix diff(m1.GetRow(), m1.GetCol());
+    
+    Matrix temp1(m1.GetRow(),m1.GetCol());
     if(m1.GetRow()==m2.GetRow() && m1.GetCol() == m2.GetCol())
     {
-    for (int i = 0; i < m1.GetRow(); i++)
-        for (int j = 0; j < m1.GetCol(); j++)
-            diff.Element(i, j) = m1.Element(i, j) - m2.Element(i, j);
-    return (diff);
+    for (int i = 0; i<m1.GetRow(); i++)
+        for (int j = 0; j<m1.GetCol(); j++)
+            temp1.Element(i,j)=m1.Element(i,j)-m2.Element(i,j);
+    return(temp1);
     }
     else 
     {
         cout<<"Enter right mathix\n";
+        return m1;
     }
 }
 
@@ -112,34 +114,46 @@ Matrix operator*(Matrix &m1, Matrix &m2)
     else
     {
         cout<<"Cannot be multiplied\n";
+        return 1;
     }
 
 }
 
 int main()
 {
-    
-    Matrix M, N(2), S(2), L(2,3);
-    /*int n, m;
-    cout<<"enter count of rows\n";
-    cin>>n;
-    cout<<"enter count of colums\n";
-    cin>>m;
-    Matrix K(n,m);
-    cout<<"enter matrix with "<<n<<" rows and "<<m<<" colums\n";
+    Matrix M, N(2),L(2,3);
+    int n1, m1, n2, m2;
+    cout<<"enter count of rows1\n";
+    cin>>n1;
+    cout<<"enter count of colums1\n";
+    cin>>m1;
+    Matrix K(n1,m1);
+    cout<<"enter matrix with "<<n1<<" rows and "<<m1<<" colums\n";
     cin>> K;
-    cout<<K;*/
+    cout<<K;
+     cout<<"enter count of rows2\n";
+    cin>>n2;
+    cout<<"enter count of colums2\n";
+    cin>>m2;
+    Matrix X(n2,m2);
+    cout<<"enter matrix with "<<n2<<" rows and "<<m2<<" colums\n";
+    cin>> X;
+    cout<<X;
+    Matrix S(n1,m2);
+    /*
     cout << "enter matrix M:\n";
     cin >> M;
     cout << M; 
     cout << "enter matrix N:\n";
     cin >> N;
     cout<< N;
-    cout<<"jnj";
-    S = M - N;
+    */
+    //cout<<"jnj";
+    
+    S = K-X;
     //L=M*K;
     cout << S;
-    cout<<"dskmcwl";
+    //cout<<"dskmcwl";
     //cout<<L;
     return 0;
 }
